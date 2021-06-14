e7065f7b12cc10dfa0b3317f6bd16dd4033ca51b
zjw <601056601@qq.com>
2021-06-15 00:13:55 +0800

    push commit

--- src/main/java/com/gdcp/scw/springsession/controller/SpringSessionController.java
+++ src/main/java/com/gdcp/scw/springsession/controller/SpringSessionController.java
@@ -6,5 +6,6 @@ public class SpringSessionController {
     public static void main(String[] args) {
         System.out.println("hahaha33344446666666");
         System.out.println("hahaha333444455555");
+        System.out.println("hahaha333444455555");
     }
 }
