2062be84b438b46c5b49d8e3caaebcc83cc5de81
zjw <601056601@qq.com>
2021-06-14 23:55:20 +0800

    hot-fix commit2

--- src/main/java/com/gdcp/scw/springsession/controller/SpringSessionController.java
+++ src/main/java/com/gdcp/scw/springsession/controller/SpringSessionController.java
@@ -4,6 +4,6 @@ import org.springframework.http.converter.json.GsonBuilderUtils;
 
 public class SpringSessionController {
     public static void main(String[] args) {
-        System.out.println("hahaha3334444");
+        System.out.println("hahaha333444455555");
     }
 }
