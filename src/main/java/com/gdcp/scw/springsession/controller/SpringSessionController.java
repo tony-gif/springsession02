691472771bd5e04c3ba8e46f5af781aa6c7e8ed9
zjw <601056601@qq.com>
2021-06-14 23:55:58 +0800

    master commit2

--- src/main/java/com/gdcp/scw/springsession/controller/SpringSessionController.java
+++ src/main/java/com/gdcp/scw/springsession/controller/SpringSessionController.java
@@ -4,6 +4,6 @@ import org.springframework.http.converter.json.GsonBuilderUtils;
 
 public class SpringSessionController {
     public static void main(String[] args) {
-        System.out.println("hahaha3334444");
+        System.out.println("hahaha33344446666666");
     }
 }
